ad5e9d7d5121f7cd4751f8d0c9edcbadbd66e843
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-15 21:58:58 +0545

    Continuing looping

--- DSA/Phase 2 (Looping & Patterns).c
+++ DSA/Phase 2 (Looping & Patterns).c
@@ -21,11 +21,36 @@
 
 #include <stdio.h>
 
+// int main() {
+//     for (int i = 1; i <= 100; i++) {
+//         if (i % 2 == 0) {
+//             printf("%d ", i);
+//         }
+//     }
+
+//     return 0;
+// }
+
+// Print all odd numbers between 1 and 100.
+
+// #include <stdio.h>
+
+// int main() {
+//     for(int i=1; i<=100; i++){
+//         if(!(i%2==0)){
+//             printf("%d",i);
+//         }
+//     }
+//     return 0;
+// }
+
+// 4. Print numbers from 10 down to 1.
+
+#include <stdio.h>
+
 int main() {
-    for (int i = 1; i <= 100; i++) {
-        if (i % 2 == 0) {
-            printf("%d ", i);
-        }
+    for (int i=10; i>=1; i--){
+        printf("%d\n", i);
     }
 
     return 0;
