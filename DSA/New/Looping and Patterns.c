17fd879b3b2a12cf52be886f388d4f55dde10797
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-16 12:48:54 +0545

    Right angled star pattern printing

--- DSA/New/Looping and Patterns.c
+++ DSA/New/Looping and Patterns.c
@@ -2,9 +2,29 @@
 
 // 1. Print a right-angled triangle of stars (n rows).
 
+
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+
 #include <stdio.h>
 
 int main() {
-    
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for (int j=1; j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
+
+
+// 
