197315d0c09333b5d55dd939023ef13da8e87ff2
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-17 20:52:33 +0545

    'Reccursion

--- DSA/New/Recursion.c
+++ DSA/New/Recursion.c
@@ -17,3 +17,58 @@ int main() {
 
     return 0;
 }
+
+// sum of 1 to n 
+
+#include <stdio.h>
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+
+    return n+sum(n-1);
+}
+int main() {
+    int result=sum(10);
+
+    printf("%d\n", result);
+    return 0;
+}
+
+// fibonacci series
+
+#include <stdio.h>
+int fibonacci(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    return fibonacci(n-1)+fibonacci(n-2); 
+}
+int main() {
+    int result=fibonacci(10);
+
+    printf("%d\n", result);
+    return 0;
+    
+}
+
+// count the number of digits recursively
+
+#include <stdio.h>
+int count(int n){
+    if(n==0){
+        return 0;
+    }
+
+    return 1+count(1/10);
+}
+int main() {
+    int result=count(10);
+    printf("%d\n", result);
+    return 0;
+}
+
