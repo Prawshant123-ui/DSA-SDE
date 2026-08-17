a4ee8d762050f87c8e74cb0cc83463aae488ef7a
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-17 20:39:49 +0545

    Recursion

---
+++ DSA/New/Recursion.c
@@ -0,0 +1,19 @@
+// Factorial
+
+#include <stdio.h>
+
+int fact(int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    return n * fact(n - 1);
+}
+
+int main() {
+    int result = fact(5);
+
+    printf("%d", result);
+
+    return 0;
+}
