e8f6523f11cb3222bdc7df42d07ce86a33a17bd6
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-17 12:50:23 +0545

    Looping and patterns

--- DSA/New/Looping and Patterns.c
+++ DSA/New/Looping and Patterns.c
@@ -13,18 +13,66 @@
 
 */
 
+// #include <stdio.h>
+
+// int main() {
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for (int j=1; j<=i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// reverse right triangle
+
+/*
+
+   *
+  **
+ ***
+****
+
+*/
+
+// #include <stdio.h>
+
+// int main() {
+//     int n=5;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-i-1;j++){
+//             printf(" ");
+//         }
+//         for(int j=0;j<i;j++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// Pyramid
+
 #include <stdio.h>
 
 int main() {
-    int n=5;
-    for(int i=1;i<=n;i++){
-        for (int j=1; j<=i;j++){
+    int n=3;
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=2*n-1;j++){
+        if(j>=n-(i-1) && j<=n+(i-1)){
             printf("*");
+        }else{
+            printf(" ");
         }
-        printf("\n");
     }
+    printf("\n");
+}
     return 0;
 }
 
 
-// 
+
