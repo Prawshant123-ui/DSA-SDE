22bc258af8eaa819ae8f7e0a094c5e92ca0f860d
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-19 17:36:10 +0545

    Pointers

--- DSA/New/Pointers.c
+++ DSA/New/Pointers.c
@@ -17,18 +17,36 @@
 // Derefrencing opertor : An operator that gives the value stored in that address (Denoted by *)
 //   Declare a pointer to an int, assign it the address of a variable, and print the value via dereferencing.
 
+// #include <stdio.h>
+
+// int main() {
+//     int variable=20;
+//     int *ptr=&variable;
+//     int  value=*(&variable); // Derefrencing 
+
+//     printf("Vaiable=%d\n",variable);
+//    printf("Address of variable=%p\n",ptr);
+//     printf("Value of the address that a pointer ptr is pointing=%d",value);
+
+
+//     return 0;
+// }
+
+
+// Change the value of a variable indirectly through its pointer
+
 #include <stdio.h>
 
 int main() {
-    int variable=20;
-    int *ptr=&variable;
-    int  value=*(&variable); // Derefrencing 
+    int var=30;
+    int *ptr=&var;
 
-    printf("Vaiable=%d\n",variable);
-   printf("Address of variable=%p\n",ptr);
-    printf("Value of the address that a pointer ptr is pointing=%d",value);
+    printf("Before:%d",var);
 
+    *ptr=20;
 
+    printf("After:%d",var);
     return 0;
 }
+
 
