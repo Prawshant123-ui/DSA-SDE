9fe62284e4d019c48c11eecce1c0080c1d42fae9
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-20 13:45:24 +0545

    Pointers

--- DSA/New/Pointers.c
+++ DSA/New/Pointers.c
@@ -49,4 +49,27 @@ int main() {
     return 0;
 }
 
+// Take two integers and swap them using a function with pointer parameters (call by reference)
 
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    printf("Before swap: a = %d, b = %d\n", a, b);
+
+    swap(&a, &b);
+
+    printf("After swap: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
