5118d9460bc7691b78e3ac677d6ddd8c19e1abc5
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-21 15:43:53 +0545

    Structures

--- DSA/New/Structures.c
+++ DSA/New/Structures.c
@@ -1,17 +1,40 @@
 // Define a struct Student with name, roll number, and marks; declare a variable and print its members.
 
 #include <stdio.h>
-#include<string.h>
 
-struct Student{
-char name[25];
-int roll_number;
-int marks;
+struct Student
+{
+    char name[25];
+    int roll_number;
+    int marks;
 };
-int main() {
-    struct Student s={"Prashant",7,98};
-    printf("Name:%s\n",s.name);
-    printf("Roll number:%d\n",s.roll_number);
+int main()
+{
+    struct Student s = {"Prashant", 7, 98};
+    printf("Name:%s\n", s.name);
+    printf("Roll number:%d\n", s.roll_number);
     printf("Marks:%d\n", s.marks);
     return 0;
 }
+
+// Use typedef to simplify a struct declaration (typedef struct {...} Point;).
+
+#include <stdio.h>
+
+typedef struct {
+    char name[25];
+    int roll_number;
+    int marks;
+} Student;
+
+int main() {
+    Student s={"Prashant", 7, 98};
+    printf("Name:%s\n", s.name);
+    printf("Roll number:%d\n", s.roll_number);
+    printf("Marks:%d\n", s.marks);
+
+    return 0;
+}
+
+// Create an array of 5 struct Student and fill it using a loop; print all records.
+
