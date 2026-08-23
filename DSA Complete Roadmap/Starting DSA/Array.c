402180c2babf6ba5d1c57496915dd09413f83378
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-23 21:27:24 +0545

    Arrays:-> Array Operations

--- DSA Complete Roadmap/Starting DSA/Array.c
+++ DSA Complete Roadmap/Starting DSA/Array.c
@@ -1,30 +1,55 @@
+// Traversal of an array
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int arr[50], size;
+//     printf("Enter the size of an array:");
+//     scanf("%d", &size);
+//     printf("Enter the elements:");
+//     for (int i = 0; i < size; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     printf("The elements in an array are :");
+//     for (int i = 0; i < size; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
+
+//Insertion of an element in any index of an array
+
+
 // Find maximum and minimum element
 
-#include <stdio.h>
-#include <limits.h>
+// #include <stdio.h>
+// #include <limits.h>
 
-int main()
-{
-    int arr[5] = {2, 4, 1, 7, 0};
-    int size = 5;
-    int max = INT_MIN;
-    int min = INT_MAX;
+// int main()
+// {
+//     int arr[5] = {2, 4, 1, 7, 0};
+//     int size = 5;
+//     int max = INT_MIN;
+//     int min = INT_MAX;
 
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] > max)
-        {
-            max = arr[i];
-        }
-        if (arr[i] < min)
-        {
-            min = arr[i];
-        }
-    }
-    printf("Maximum:%d\n", max);
-    printf("Minimum:%d\n", min);
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (arr[i] > max)
+//         {
+//             max = arr[i];
+//         }
+//         if (arr[i] < min)
+//         {
+//             min = arr[i];
+//         }
+//     }
+//     printf("Maximum:%d\n", max);
+//     printf("Minimum:%d\n", min);
 
-    return 0;
-}
+//     return 0;
+// }
 
-
+// Reverse an array.
