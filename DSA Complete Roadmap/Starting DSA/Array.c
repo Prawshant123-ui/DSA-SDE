8e94f8ea82eca49c69533aea3b11a681a6cf6603
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-25 15:10:11 +0545

    Array:--> Deletion of an element from any position in an array

--- DSA Complete Roadmap/Starting DSA/Array.c
+++ DSA Complete Roadmap/Starting DSA/Array.c
@@ -71,24 +71,44 @@
 
 // Insertion of an element at end  of an array
 
+// #include <stdio.h>
+
+// int main() {
+//      int arr[50]={1,2,3,4,5};
+//     int size=5;
+//     int val=6;
+
+//     arr[size]=val;
+//     size++;
+
+//      printf("The new array is:");
+//      for(int i=0;i<size;i++){
+//         printf("%d ",arr[i]);
+//      }
+//     return 0;
+// }
+
+
+// Deletion of an element from any index in an array
+
 #include <stdio.h>
 
 int main() {
-     int arr[50]={1,2,3,4,5};
+    int arr[50]={1,2,3,4,5};
     int size=5;
-    int val=6;
+    int idx=2;
 
-    arr[size]=val;
-    size++;
+    for(int i=idx;i<size-1;i++){
+        arr[i]=arr[i+1];
+    }
+    size--;
 
-     printf("The new array is:");
-     for(int i=0;i<size;i++){
+    printf("The final array is :");
+    for(int i=0;i<size;i++){
         printf("%d ",arr[i]);
-     }
+    }
     return 0;
 }
-
-
 
 
 
