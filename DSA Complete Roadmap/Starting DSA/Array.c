5e86169c7bb804632276fcd685d765c654a4a66b
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-26 22:28:09 +0545

    Array:--> Reversing an array

--- DSA Complete Roadmap/Starting DSA/Array.c
+++ DSA Complete Roadmap/Starting DSA/Array.c
@@ -233,50 +233,86 @@
 
 // Updating an element in an array
 
+// #include <stdio.h>
+
+// int update(int arr[], int size, int value, int idx)
+// {
+//     if (idx >= 0 && idx < size)
+//     {
+//         arr[idx] = value;
+//         return idx;
+//     }
+//     else
+//     {
+//         printf("Invalid index\n");
+//         return -1;
+//     }
+// }
+
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+
+//     int value = 9;
+//     int idx = 1;
+
+//     int result = update(arr, size, value, idx);
+
+//     if (result == -1)
+//     {
+//         printf("Element not updated\n");
+//     }
+//     else
+//     {
+//         printf("Element is updated as %d at index %d\n", value, result);
+
+//         printf("Updated Array: ");
+
+//         for (int i = 0; i < size; i++)
+//         {
+//             printf("%d ", arr[i]);
+//         }
+//     }
+
+//     return 0;
+// }
+
+//Reversing an array
+
+
 #include <stdio.h>
 
-int update(int arr[], int size, int value, int idx)
-{
-    if (idx >= 0 && idx < size)
-    {
-        arr[idx] = value;
-        return idx;
-    }
-    else
-    {
-        printf("Invalid index\n");
-        return -1;
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+    int temp;
+
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
     }
 }
 
-int main()
-{
+int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int value = 9;
-    int idx = 1;
+    reverseArray(arr, size);
 
-    int result = update(arr, size, value, idx);
+    printf("Reversed array: ");
 
-    if (result == -1)
-    {
-        printf("Element not updated\n");
-    }
-    else
-    {
-        printf("Element is updated as %d at index %d\n", value, result);
-
-        printf("Updated Array: ");
-
-        for (int i = 0; i < size; i++)
-        {
-            printf("%d ", arr[i]);
-        }
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
     }
 
     return 0;
 }
+
 
 
 
