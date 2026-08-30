7838a3abc9254363b0ecfb6ac81fd23a0a0636a2
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-08-30 16:06:22 +0545

    Practice questions

--- DSA Complete Roadmap/Starting DSA/Practice questions.c
+++ DSA Complete Roadmap/Starting DSA/Practice questions.c
@@ -92,9 +92,8 @@ int main()
 
     printf("The rotated array is :");
 
-    for (int i = 0; i < size, i++);
-    {
-        printf("%d ", arr[i]);
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
     }
     return 0;
 }
