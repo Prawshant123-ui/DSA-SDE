d2f60c0e96ab8e27783f4d5ccfdf57de820c37d4
Prawshant123-ui <coddieprawshant909@gmail.com>
2026-09-09 21:06:01 +0545

    Check if the array is sorted or not

--- DSA Complete Roadmap/Striver DSA/Array.easy.cpp
+++ DSA Complete Roadmap/Striver DSA/Array.easy.cpp
@@ -32,36 +32,63 @@
 
 // Second Largest Element
 
+// #include <iostream>
+// using namespace std;
+// #include <vector>
+
+// int secondLargest(vector<int> &arr, int n)
+// {
+//     int largest = arr[0];
+//     int slargest = -1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] > largest)
+//         {
+//             slargest = largest;
+//             largest = arr[i];
+//         }
+//         else if (arr[i] < largest && arr[i] > slargest)
+//         {
+//             slargest = arr[i];
+//         }
+//     }
+//     return slargest;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4, 5};
+//     int n = arr.size();
+//     int result = secondLargest(arr, n);
+
+//     cout << "The 2nd largest element in the array is: " << result;
+
+//     return 0;
+// }
+
+
+// Check if the given array is sorted
+
 #include <iostream>
 using namespace std;
 #include <vector>
 
-int secondLargest(vector<int> &arr, int n)
-{
-    int largest = arr[0];
-    int slargest = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > largest)
-        {
-            slargest = largest;
-            largest = arr[i];
-        }
-        else if (arr[i] < largest && arr[i] > slargest)
-        {
-            slargest = arr[i];
+bool checkSorted(vector<int>& arr, int n) {
+    for (int i = 1; i < n; i++) {
+        if (arr[i] <= arr[i - 1]) {
+            return false;
         }
     }
-    return slargest;
+    return true;
 }
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5};
+     vector<int> arr = {1, 2, 3, 4, 5};
     int n = arr.size();
-    int result = secondLargest(arr, n);
 
-    cout << "The 2nd largest element in the array is: " << result;
-
+    int result=checkSorted(arr,n);
+    cout << "The array is sorted or not? " << result;
+    
     return 0;
 }
